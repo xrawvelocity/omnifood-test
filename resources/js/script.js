f68d35c30342a91e9ee76e9dcc8c43dd1005abae
Victor Fernandez <vmfs99@gmail.com>
2019-12-06 19:26:34 -0500

    added files

---
+++ resources/js/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+    // For the sticky nav
+    $('.js--section-features').waypoint(function(direction) {
+      if (direction === 'down') {
+        $('nav').addClass('sticky');
+      }
+      else {
+        $('nav').removeClass('sticky');
+      }
+    }, {
+      offset: '60px;'
+    });
+
+    // Scroll on buttons
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1500);
+    });
+
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    // Animations on scroll
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated tada');
+    }, {
+        offset: '50%'
+    });
+
+
+});
+
+
+// Mobile nav
+$('#js--nav-icon').click(function() {
+    var nav = $('.main-nav');
+
+    nav.slideToggle(200);
+
+    var x = document.getElementById("js--nav-icon").attributes[1].value;
+
+    if(x == "menu"){
+        $("#js--nav-icon").attr("name", "close");
+    }
+    else{
+        $("#js--nav-icon").attr("name", "menu");
+    }
+});
